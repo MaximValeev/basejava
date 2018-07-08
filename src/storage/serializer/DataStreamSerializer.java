e073b676e8625c69f5dfdf69fe521cbaa2ed104d
MaximValeev <fromghetto94@gmail.com>
2018-07-08 23:53:54 +1000

    HW_10. Reduced number of nested loops in DataStreamSerializer via anonymous classes and lambda expressions.

--- src/storage/serializer/DataStreamSerializer.java
+++ src/storage/serializer/DataStreamSerializer.java
@@ -4,8 +4,9 @@ import model.*;
 
 import java.io.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public class DataStreamSerializer implements StreamSerializer {
 
@@ -14,52 +15,37 @@ public class DataStreamSerializer implements StreamSerializer {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
-            Map<ContactType, String> contacts = resume.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> contactEntry : resume.getContacts().entrySet()) {
-                dos.writeUTF(contactEntry.getKey().name());
-                dos.writeUTF(contactEntry.getValue());
-            }
-
-            //TODO implements section
-
-            Map<SectionType, Section> sections = resume.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                String sectionName = entry.getKey().name();
+            writeSequence(dos, resume.getContacts().entrySet(), contactsEntry -> {
+                dos.writeUTF(contactsEntry.getKey().name());
+                dos.writeUTF(contactsEntry.getValue());
+            });
+            writeSequence(dos, resume.getSections().entrySet(), sectionsEntry -> {
+                String sectionName = sectionsEntry.getKey().name();
                 dos.writeUTF(sectionName);
                 switch (SectionType.valueOf(sectionName)) {
                     case PERSONAL:
                     case OBJECTIVE:
-                        dos.writeUTF(((SectionText) entry.getValue()).getContent());
+                        dos.writeUTF(((SectionText) sectionsEntry.getValue()).getContent());
                         break;
                     case QUALIFICATIONS:
                     case ACHIEVEMENTS:
-                        List<String> items = ((SectionItemsList) entry.getValue()).getItems();
-                        dos.writeInt(items.size());
-                        for (String item : items) {
-                            dos.writeUTF(item);
-                        }
+                        writeSequence(dos, ((SectionItemsList) sectionsEntry.getValue()).getItems(), dos::writeUTF);
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
-                        List<Place> places = ((SectionPlace) entry.getValue()).getPlaces();
-                        dos.writeInt(places.size());
-                        for (Place place : places) {
-                            List<Place.WorkPosition> workPositions = place.getWorkPositions();
-                            dos.writeInt(workPositions.size());
-                            for (Place.WorkPosition workPosition : workPositions) {
+                        writeSequence(dos, ((SectionPlace) sectionsEntry.getValue()).getPlaces(), place -> {
+                            writeSequence(dos, place.getWorkPositions(), workPosition -> {
                                 dos.writeUTF(workPosition.getTitle());
                                 dos.writeUTF(workPosition.getDescription());
                                 writeDate(dos, workPosition.getStartDate());
                                 writeDate(dos, workPosition.getEndDate());
-                            }
+                            });
                             dos.writeUTF(place.getHomePage().getName());
                             dos.writeUTF(place.getHomePage().getUrl());
-                        }
+                        });
                         break;
                 }
-            }
+            });
         }
     }
 
@@ -67,48 +53,36 @@ public class DataStreamSerializer implements StreamSerializer {
     public Resume doRead(InputStream is) throws IOException {
         try (DataInputStream dis = new DataInputStream(is)) {
             Resume resume = new Resume(dis.readUTF(), dis.readUTF());
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-            //TODO implements section
+            readSequence(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
 
-            int sectionCount = dis.readInt();
-            for (int i = 0; i < sectionCount; i++) {
-                String sectionName = dis.readUTF();
-                switch (SectionType.valueOf(sectionName)) {
+            readSequence(dis, () -> {
+                SectionType section = SectionType.valueOf(dis.readUTF());
+                switch (section) {
                     case PERSONAL:
                     case OBJECTIVE:
-                        String content = dis.readUTF();
-                        resume.addSection(SectionType.valueOf(sectionName), new SectionText(content));
+                        resume.addSection(section, new SectionText(dis.readUTF()));
                         break;
                     case ACHIEVEMENTS:
                     case QUALIFICATIONS:
-                        int countItems = dis.readInt();
-                        String[] items = new String[countItems];
-                        for (int j = 0; j < countItems; j++) {
-                            items[j] = dis.readUTF();
-                        }
-                        resume.addSection(SectionType.valueOf(sectionName), new SectionItemsList(items));
+                        List<String> items = new ArrayList<>();
+                        readSequence(dis, () -> items.add(dis.readUTF()));
+                        resume.addSection(section, new SectionItemsList(items));
                         break;
-                    case EXPERIENCE:
                     case EDUCATION:
-                        int countPlaces = dis.readInt();
-                        Place[] places = new Place[countPlaces];
-                        for (int k = 0; k < countPlaces; k++) {
-                            int countPositions = dis.readInt();
-                            Place.WorkPosition[] workPositions = new Place.WorkPosition[countPositions];
-                            for (int l = 0; l < countPositions; l++) {
-                                workPositions[l] = new Place.WorkPosition(dis.readUTF(), dis.readUTF(),
-                                        LocalDate.of(dis.readInt(), dis.readInt(), 1),
-                                        LocalDate.of(dis.readInt(), dis.readInt(), 1));
-                            }
-                            places[k] = new Place(dis.readUTF(), dis.readUTF(), workPositions);
-                        }
-                        resume.addSection(SectionType.valueOf(sectionName), new SectionPlace(places));
+                    case EXPERIENCE:
+                        List<Place> places = new ArrayList<>();
+                        readSequence(dis, () -> {
+                            List<Place.WorkPosition> workPositions = new ArrayList<>();
+                            readSequence(dis, () ->
+                                    workPositions.add(new Place.WorkPosition(dis.readUTF(), dis.readUTF(),
+                                            LocalDate.of(dis.readInt(), dis.readInt(), 1),
+                                            LocalDate.of(dis.readInt(), dis.readInt(), 1))));
+                            places.add(new Place(new Link(dis.readUTF(), dis.readUTF()), workPositions));
+                        });
+                        resume.addSection(section, new SectionPlace(places));
                         break;
                 }
-            }
+            });
             return resume;
         }
     }
@@ -116,5 +90,27 @@ public class DataStreamSerializer implements StreamSerializer {
     private void writeDate(DataOutputStream dos, LocalDate date) throws IOException {
         dos.writeInt(date.getYear());
         dos.writeInt(date.getMonth().getValue());
+    }
+
+    private interface unitWriter<T> {
+        void write(T unit) throws IOException;
+    }
+
+    private interface unitReader {
+        void read() throws IOException;
+    }
+
+    private <T> void writeSequence(DataOutputStream dos, Collection<T> collection, unitWriter<T> writer) throws IOException {
+        dos.writeInt(collection.size());
+        for (T unit : collection) {
+            writer.write(unit);
+        }
+    }
+
+    private void readSequence(DataInputStream dis, unitReader reader) throws IOException {
+        int count = dis.readInt();
+        for (int i = 0; i < count; i++) {
+            reader.read();
+        }
     }
 }
