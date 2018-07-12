7e052fa9d44cb73858cf3c56201e81c19adb016e
MaximValeev <fromghetto94@gmail.com>
2018-07-12 19:25:41 +1000

    HW_11. Realized deadlock in MainConcurrency.java

--- src/MainConcurrency.java
+++ src/MainConcurrency.java
@@ -1,27 +1,42 @@
 public class MainConcurrency {
+    private static final Object object1 = new Object();
+    private static final Object object2 = new Object();
+
     public static void main(String[] args) {
-        new Thread(new TestThread()).start();
-        new Thread(new TestThread()).start();
-    }
 
-    static class TestThread implements Runnable {
-        static final Object lockObject = new Object();
-
-        @Override
-        public void run() {
-            synchronized (lockObject) {
-                System.out.println(Thread.currentThread().getName() + " in sync block");
-                try {
-                    System.out.println(Thread.currentThread().getName() + " in try block");
-                    lockObject.wait();
-                    System.out.println(Thread.currentThread().getName() + " after wait");
-                    lockObject.notify();
-                    System.out.println(Thread.currentThread().getName() + " after notify");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        Thread firstThread = new Thread(() -> {
+            synchronized (object1) {
+                locked("obj1");
+                Thread.yield();
+                synchronized (object2) {
+                    locked("obj2");
                 }
+                unlocked("obj2");
             }
-        }
+            unlocked("obj1");
+        });
+
+        Thread secondThread = new Thread(() -> {
+            synchronized (object2) {
+                locked("obj2");
+                Thread.yield();
+                synchronized (object1) {
+                    locked("obj1");
+                }
+                unlocked("obj1");
+            }
+            unlocked("obj2");
+        });
+
+        firstThread.start();
+        secondThread.start();
     }
 
+    static void unlocked(String objName) {
+        System.out.println(Thread.currentThread().getName() + " unlocked " + objName);
+    }
+
+    static void locked(String objName) {
+        System.out.println(Thread.currentThread().getName() + " locked on " + objName);
+    }
 }
